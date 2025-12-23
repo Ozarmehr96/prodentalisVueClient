fe12ec5fd228277ffab6d9a5aed441013f4997d6
Ozar <odilov090996@mail.ru>
2025-12-23 22:34:24 +0300

    Добавлен тип цены этапа работы (за один зуб, за этап). Добавлена кнопка выхода из аккаунта в профиле сотрудника.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -162,6 +162,15 @@
           </div>
         </div>
       </div>
+
+      <div class="mb-3 border-0">
+        <!-- Кнопка загрузки -->
+        <div class="col-12 text-center py-2">
+          <button class="btn btn-outline-danger w-100" @click="() => logOut()">
+            Выйти из аккаунта
+          </button>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -173,6 +182,7 @@ import {
   IS_STATS_LOADING,
   LOAD_ROLES,
   LOAD_USER_STAT,
+  LOGOUT,
   ROLES,
   USER_STAT,
 } from "../store/types";
@@ -271,6 +281,7 @@ export default {
     ...mapActions({
       loadRoles: LOAD_ROLES,
       loadUserStatAction: LOAD_USER_STAT,
+      logOut: LOGOUT,
     }),
 
     // Формат денег
