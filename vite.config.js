db36ef6ef092d645209d57d0fa8a361369273328
Ozar <odilov090996@mail.ru>
2026-01-01 21:24:36 +0300

    Добавлен PWA.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import pkg from './package.json';
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/', // ✅ абсолютные пути к ассетам
   define: {
@@ -9,6 +10,33 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',        // Автообновление SW
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'ProDentalis',
+        short_name: 'ProDentalis',
+        description: 'Автоматизация зуботехнической лаборатории',
+        theme_color: '#3dd2cc', // цвет браузерной строки
+        background_color: '#ffffff', // цвет сплэш-скрина
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          { src: '/pwa-logo.png', sizes: '192x192', type: 'image/png' },
+          // { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/pwa-logo.png', sizes: '512x512', type: 'image/png' }
+        ]
+      },
+      workbox: {
+        // ⚠️ Важно: не кэшируем API-запросы
+        runtimeCaching: [],  
+        cleanupOutdatedCaches: true // автоматически удаляет старые кеши
+      },
+      devOptions: {
+        enabled: true,   // чтобы PWA работало на localhost
+        type: 'module'
+      }
+    }),
     Pages({
       dirs: 'src/pages', // где лежат страницы
       extensions: ['vue'], // расширение
